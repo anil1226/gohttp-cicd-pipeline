2c498e25097fcf7cd2e6852eff3812481e77c377
Anil Karuturi <anilchowdary1226@gmail.com>
2024-04-01 12:35:13 -0400

    update GET

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /todos", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "get all todos")
+		fmt.Fprintf(w, "get todos")
 	})
 
 	http.ListenAndServe(":8080", router)
